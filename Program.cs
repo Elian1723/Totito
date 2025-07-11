3864f7ca579cb0294249c5d4d43cd6925828c050
Elián Barrios <elianbarrios1707@outlook.com>
2025-07-11 10:04:59 -0600

    Agregada la clase Totio para separar la logica del juego

--- Program.cs
+++ Program.cs
@@ -18,7 +18,8 @@
                     switch (opcion)
                     {
                         case 1:
-                            ComenzarJuego();
+                            var totito = new Totito();
+                            totito.Jugar();
                             break;
                         case 2:
                             break;
@@ -72,53 +73,6 @@
 
             Console.SetCursorPosition(margenIzquierdo + 2, margenSuperior + 7);
             Console.Write("Elija una opción: ");
-        }
-
-        static void ComenzarJuego()
-        {
-            Console.Clear();
-            DibujarTablero();
-
-            Console.SetCursorPosition(7, 4);
-            var t = Console.ReadKey();
-
-            if (t.Key == ConsoleKey.Escape)
-            {
-
-            }
-        }
-
-        static void DibujarTablero(int margenIzquierdo = 5, int margenSuperior = 1)
-        {
-            Console.SetCursorPosition(margenIzquierdo, margenSuperior + 2);
-            Console.WriteLine(new string('-', 17));
-
-            Console.SetCursorPosition(margenIzquierdo, margenSuperior + 4);
-            Console.WriteLine(new string('-', 17));
-
-            for (int i = 0; i < 7; i++)
-            {
-                Console.SetCursorPosition(margenIzquierdo + 5, margenSuperior + i);
-                Console.WriteLine("|");
-
-                Console.SetCursorPosition(margenIzquierdo + 11, margenSuperior + i);
-                Console.WriteLine("|");
-            }
-
-            Console.SetCursorPosition(margenIzquierdo + 25, margenSuperior + 1);
-            Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine("X jugador 1");
-
-            Console.SetCursorPosition(margenIzquierdo + 25, margenSuperior + 2);
-            Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine("O jugador 2");
-
-            Console.ResetColor();
-            Console.SetCursorPosition(margenIzquierdo + 25, margenSuperior + 4);
-            Console.WriteLine("Presione ESC para volver al menú");
-
-            Console.SetCursorPosition(margenIzquierdo + 25, margenSuperior + 6);
-            Console.WriteLine("Turno del jugador 1");
         }
     }
 }
---
+++ Totito.cs
@@ -0,0 +1,54 @@
+﻿namespace Totito;
+
+public class Totito
+{
+    private const int _margenIzquierdo = 5;
+    private const int _margenSuperior = 1;
+
+    public void Jugar()
+    {
+        Console.Clear();
+        DibujarTablero();
+
+        Console.SetCursorPosition(_margenIzquierdo + 2, _margenSuperior + 1);
+        var t = Console.ReadKey();
+
+        if (t.Key == ConsoleKey.Escape)
+        {
+
+        }
+    }
+
+    private void DibujarTablero()
+    {
+        Console.SetCursorPosition(_margenIzquierdo, _margenSuperior + 2);
+        Console.WriteLine(new string('-', 17));
+
+        Console.SetCursorPosition(_margenIzquierdo, _margenSuperior + 4);
+        Console.WriteLine(new string('-', 17));
+
+        for (int i = 0; i < 7; i++)
+        {
+            Console.SetCursorPosition(_margenIzquierdo + 5, _margenSuperior + i);
+            Console.WriteLine("|");
+
+            Console.SetCursorPosition(_margenIzquierdo + 11, _margenSuperior + i);
+            Console.WriteLine("|");
+        }
+
+        Console.SetCursorPosition(_margenIzquierdo + 25, _margenSuperior + 1);
+        Console.ForegroundColor = ConsoleColor.Red;
+        Console.WriteLine("X jugador 1");
+
+        Console.SetCursorPosition(_margenIzquierdo + 25, _margenSuperior + 2);
+        Console.ForegroundColor = ConsoleColor.Blue;
+        Console.WriteLine("O jugador 2");
+
+        Console.ResetColor();
+        Console.SetCursorPosition(_margenIzquierdo + 25, _margenSuperior + 4);
+        Console.WriteLine("Presione ESC para volver al menú");
+
+        Console.SetCursorPosition(_margenIzquierdo + 25, _margenSuperior + 6);
+        Console.WriteLine("Turno del jugador 1");
+    }
+}
