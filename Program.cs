fd015a9b811725fec2a480227a00079cc1cc3cfe
Elián Barrios <elianbarrios1707@outlook.com>
2025-07-11 09:53:55 -0600

    Creado el tablero del juego

--- Program.cs
+++ Program.cs
@@ -4,7 +4,44 @@
     {
         static void Main(string[] args)
         {
-            MostrarMenu();
+            int opcion = 0;
+
+            while (opcion != 3)
+            {
+                Console.Clear();
+                DibujarMenu();
+
+                string? input = Console.ReadLine();
+
+                if (int.TryParse(input, out opcion))
+                {
+                    switch (opcion)
+                    {
+                        case 1:
+                            ComenzarJuego();
+                            break;
+                        case 2:
+                            break;
+                        case 3:
+                            break;
+                        default:
+                            Console.SetCursorPosition(5, 10);
+                            Console.WriteLine("Ingrese una opción válida");
+                            break;
+                    }
+                }
+                else
+                {
+                    Console.SetCursorPosition(5, 10);
+                    Console.WriteLine("Ingrese una opción válida");
+                }
+
+                Console.SetCursorPosition(5, 10);
+                Console.Write("Presione cualquier tecla para continuar...");
+                Console.ReadKey();
+            }
+            
+            Console.Clear();
         }
 
         static void DibujarMenu(int margenIzquierdo = 5, int margenSuperior = 1)
@@ -37,45 +74,51 @@
             Console.Write("Elija una opción: ");
         }
 
-        static void MostrarMenu()
+        static void ComenzarJuego()
         {
-            int opcion = 0;
+            Console.Clear();
+            DibujarTablero();
 
-            while (opcion != 3)
+            Console.SetCursorPosition(7, 4);
+            var t = Console.ReadKey();
+
+            if (t.Key == ConsoleKey.Escape)
             {
-                Console.Clear();
-                DibujarMenu();
 
-                string? input = Console.ReadLine();
-
-                if (int.TryParse(input, out opcion))
-                {
-                    switch (opcion)
-                    {
-                        case 1:
-                            break;
-                        case 2:
-                            break;
-                        case 3:
-                            Console.SetCursorPosition(5, 10);
-                            Console.WriteLine("¡Nos vemos!");
-                            break;
-                        default:
-                            Console.SetCursorPosition(5, 10);
-                            Console.WriteLine("Ingrese una opción válida");
-                            break;
-                    }
-                }
-                else
-                {
-                    Console.SetCursorPosition(5, 10);
-                    Console.WriteLine("Ingrese una opción válida");
-                }
-
-                Console.SetCursorPosition(5, 10);
-                Console.Write("Presione cualquier tecla para continuar...");
-                Console.ReadKey();
             }
+        }
+
+        static void DibujarTablero(int margenIzquierdo = 5, int margenSuperior = 1)
+        {
+            Console.SetCursorPosition(margenIzquierdo, margenSuperior + 2);
+            Console.WriteLine(new string('-', 17));
+
+            Console.SetCursorPosition(margenIzquierdo, margenSuperior + 4);
+            Console.WriteLine(new string('-', 17));
+
+            for (int i = 0; i < 7; i++)
+            {
+                Console.SetCursorPosition(margenIzquierdo + 5, margenSuperior + i);
+                Console.WriteLine("|");
+
+                Console.SetCursorPosition(margenIzquierdo + 11, margenSuperior + i);
+                Console.WriteLine("|");
+            }
+
+            Console.SetCursorPosition(margenIzquierdo + 25, margenSuperior + 1);
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("X jugador 1");
+
+            Console.SetCursorPosition(margenIzquierdo + 25, margenSuperior + 2);
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("O jugador 2");
+
+            Console.ResetColor();
+            Console.SetCursorPosition(margenIzquierdo + 25, margenSuperior + 4);
+            Console.WriteLine("Presione ESC para volver al menú");
+
+            Console.SetCursorPosition(margenIzquierdo + 25, margenSuperior + 6);
+            Console.WriteLine("Turno del jugador 1");
         }
     }
 }
